5fd53756cdd36b9cf9d80669cefea4f676f3444e
PTT154 <nguyenminhphuoc154@gmail.com>
2025-12-10 09:58:29 +0700

    fix loi p3

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -50,7 +50,7 @@ const routes = [
         nested: [
             {
                 path: "dashboard",
-                element: lazy(() => import("../pages/AdminTemplate/DashBoard")),
+                element: lazy(() => import("../pages/AdminTemplate/Dashboard")),
             },
             {
                 path: "add-user",
